ee9aa0c73edd01275c6e0fdf3cc49587f4f744fa
sulaiman-khan <sallokhan01999@gmail.com>
2025-04-17 14:39:34 +0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+var swiper = new Swiper(".mySwiper", {
+    autoplay: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+  });
+var swiper = new Swiper(".myTestmonials", {
+    autoplay: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+  });
